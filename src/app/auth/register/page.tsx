2555c8e787a4b483eeec0742931fb342bf41b165
mimi <62527898+HM970506@users.noreply.github.com>
2023-10-10 20:38:40 +0900

    design: register 화면 ui 구성

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+"use client";
 
+import React from "react";
+import { Button, Form, Input } from "antd";
+
+//회원가입과 같이 사용자 interaction이 있는 곳은 csr로!
+//ssr은 사용자와 intercation할 수 없다.
+//거기다 왜인지..form.item이 next13에서 에러가 난다. form 태그 고유의 현상인가? 아니면 해당 컴포넌트의 문제인가?
 function Register() {
-  return <div>여기는 레지스터</div>;
+  return (
+    <div className="flex justify-center align-top">
+      <div className="relative top-10 text-center gap-5 flex flex-col w-1/4">
+        <h1>Register</h1>
+        <Form>
+          <Form.Item name="name">
+            <Input placeholder={"Name"} />
+          </Form.Item>
+          <Form.Item name="email">
+            <Input placeholder={"Email"} />
+          </Form.Item>
+          <Form.Item name="password">
+            <Input.Password placeholder={"Password"} />
+          </Form.Item>
+        </Form>
+        <Button type="primary" htmlType="submit">
+          Register
+        </Button>
+        <div className="flex justify-center align-middle">
+          <Button type="link">Already have an accounts</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
