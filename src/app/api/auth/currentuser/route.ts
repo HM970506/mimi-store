a598f8ec761c09fde5dfcb92ba202a6305b8cd56
mimi <62527898+HM970506@users.noreply.github.com>
2023-10-15 01:06:32 +0900

    feat: 파싱한 토큰으로 db가져오는 함수 추가

---
+++ src/app/api/auth/currentuser/route.ts
@@ -0,0 +1,16 @@
+import { connectDB } from "@/configs/dbConfigs";
+import { validateJWT } from "@/helpers/validateJWT";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+connectDB();
+export async function GET(request: NextRequest) {
+  try {
+    const userId = await validateJWT(request);
+    //findone하고 findbyid의 차이점이 뭐지?
+    const user = await User.findById(userId).select("-password"); //pw는 제외하고 받는다는 옵션
+
+    return NextResponse.json({ data: user });
+  } catch (e: any) {
+    return NextResponse.json({ message: e.message }, { status: 400 });
+  }
+}
