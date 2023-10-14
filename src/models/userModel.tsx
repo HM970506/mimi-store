ee16845da5655fdaec290cf4fa019e1c77baf4e0
mimi <62527898+HM970506@users.noreply.github.com>
2023-10-14 18:47:13 +0900

    feat: db 초기 스키마 생성

---
+++ src/models/userModel.tsx
@@ -0,0 +1,23 @@
+//사용자 주문, 제품, 카테고리 등의 db table 모델을 생성
+
+import mongoose, { mongo } from "mongoose";
+
+export const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    deliveryAddresses: { type: Array, default: [], required: false },
+    isActive: { type: Boolean, default: true, required: false }, //결제방법
+    isAdmin: { type: Boolean, default: false, required: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.models["users"]
+  ? (module.exports = mongoose.models["users"])
+  : (module.exports = mongoose.model("users", userSchema));
+
+export default User;
