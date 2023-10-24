c5d28ed30b388d09bcd994fc2cc9c6cb5d1daec8
mimi <62527898+HM970506@users.noreply.github.com>
2023-10-24 21:12:46 +0900

    feat: product 추가 로직

--- src/app/api/products/set/route.ts
+++ src/app/api/products/set/route.ts
@@ -1,9 +1,7 @@
 import { connectDB } from "@/configs/dbConfigs";
-import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
-import bcrypt from "bcryptjs";
-import Category from "@/models/categoryModel";
 import { validateJWT } from "@/helpers/validateJWT";
+import Product from "@/models/productModel";
 
 connectDB();
 
@@ -14,13 +12,10 @@ export async function POST(request: NextRequest) {
 
     const reqBody = await request.json();
 
-    const categoryExists = await Category.findOne({ name: reqBody.name });
-    if (categoryExists) throw new Error("존재하는 카테고리입니다");
+    const newProduct = new Product({ ...reqBody, created: userId });
+    await newProduct.save();
 
-    const newCategory = new Category({ ...reqBody, created: userId });
-    await newCategory.save();
-
-    return NextResponse.json({ message: "카테고리 추가 완료" });
+    return NextResponse.json({ message: "제품 추가 완료" });
   } catch (e: any) {
     return NextResponse.json({ message: e.message }, { status: 404 });
   }
