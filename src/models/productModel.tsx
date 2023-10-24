1919b2bf1e32213eb6d528abf8667f69fbd5ae69
mimi <62527898+HM970506@users.noreply.github.com>
2023-10-24 19:11:38 +0900

    feat: product schema생성

---
+++ src/models/productModel.tsx
@@ -0,0 +1,31 @@
+//사용자 주문, 제품, 카테고리 등의 db table 모델을 생성
+
+import mongoose from "mongoose";
+
+export const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "categories",
+      require: true,
+    },
+    description: { type: String, require: true },
+    price: { type: Number, require: true },
+    stock: { type: Number, required: true, default: 100 },
+    images: { type: Array, reqiure: true, default: [] },
+    created: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product =
+  mongoose.models["products"] || mongoose.model("products", productSchema);
+
+export default Product;
